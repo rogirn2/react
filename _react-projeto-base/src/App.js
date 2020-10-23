862ee5d6f196722427535513f4afee9621b96073
Igor Brito Alves <igor.alves@arenadunas.com.br>
2020-10-22 22:15:57 -0300

    projetinho react

--- _react-projeto-base/src/App.js
+++ _react-projeto-base/src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import { getNewTimestamp } from './helpers/dateTimeHelpers';
 
 export default class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      clickArray: [],
+    };
+  }
+
+  handleClick = () => {
+    const newClickArray = Object.assign([], this.state.clickArray);
+    newClickArray.push(getNewTimestamp());
+    this.setState({ clickArray: newClickArray });
+  }
+
   render() {
-    return <ProjetoBase />;
+    const { clickArray } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.handleClick}>Clique aqui</button>
+
+        <ul>
+          {clickArray.map((item) => {
+            return <li key={item}>{ item }</li>;
+          })}
+        </ul>
+      </div>
+    );
   }
 }
